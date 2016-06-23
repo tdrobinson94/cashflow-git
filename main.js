5c429b295c03cbe8ced91d206e2f3dd611cd5ddc
tdrobinson94 <tdrobinson94@gmail.com>
2016-06-22 23:35:01 -0400

    Added animation to navbar when scrolling

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-
+$(window).scroll(function(){
+  var top = $(window).scrollTop();
+  if (top > 2){
+    $('.navbar').addClass('black_background');
+  }
+  else {
+    $('.navbar').removeClass('black_background');
+  }
+})
